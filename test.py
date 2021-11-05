78d5d7b2f826aa1111c4116d2aec9b889fcd9174
AshwiniNaik265 <savitashwini2605@gmail.com>
2021-11-05 18:44:02 +0530

    modified test

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
-
-file1 = open('myfile.txt', 'w')
-L = ["This is Delhi \n", "This is Paris \n", "This is London \n"]
-s = "Hello\n"
+#file1 = open('myfile.txt', 'w')
+#L = ["This is Delhi \n", "This is Paris \n", "This is London \n"]
+#s = "Hello\n"
   
 # Writing a string to file
-file1.write(s)
+#file1.write(s)
+print("Hello World")
